32d39d980e52387c2d75785c1b56359a6e1a56bb
Héctor Romero López <hromerol@MacBook-Pro-de-Hector.local>
2023-02-28 12:30:51 +0100

    ADD: Dialog

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function toggleNav() {
 burger.addEventListener('click', function() {
     toggleNav();
 });
+
+/*---------------------------------------------*/
+
+var updateButton = document.getElementById('updateDetails');
+var cancelButton = document.getElementById('cancel');
+var favDialog = document.getElementById('favDialog');
+
+// Update button opens a modal dialog
+updateButton.addEventListener('click', function() {
+      favDialog.showModal();
+});
+
+// Form cancel button closes the dialog box
+cancelButton.addEventListener('click', function() {
+    favDialog.close();
+});
+
